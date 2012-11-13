689b9255dfbe351842a3baf9b6c1e7ec525d08bc
mattn <mattn.jp@gmail.com>
2012-11-13 13:27:00 +0900

    trackers.

---
+++ trackers.go
@@ -0,0 +1,36 @@
+package redmine
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strings"
+)
+
+type trackersResult struct {
+	Trackers []IdName `json:"trackers"`
+}
+
+func (c *client) Trackers() ([]IdName, error) {
+	res, err := http.Get(c.endpoint + "/trackers.json?key=" + c.apikey)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	decoder := json.NewDecoder(res.Body)
+	var r trackersResult
+	if res.StatusCode != 200 {
+		var er errorsResult
+		err = decoder.Decode(&er)
+		if err == nil {
+			err = errors.New(strings.Join(er.Errors, "\n"))
+		}
+	} else {
+		err = decoder.Decode(&r)
+	}
+	if err != nil {
+		return nil, err
+	}
+	return r.Trackers, nil
+}
