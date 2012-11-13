7ff878bea060bee19377e97d09e8cea7ddbb0bac
mattn <mattn.jp@gmail.com>
2012-11-13 13:25:48 +0900

    add roles.

---
+++ roles.go
@@ -0,0 +1,36 @@
+package redmine
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strings"
+)
+
+type rolesResult struct {
+	Roles []IdName `json:"roles"`
+}
+
+func (c *client) Roles() ([]IdName, error) {
+	res, err := http.Get(c.endpoint + "/roles.json?key=" + c.apikey)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	decoder := json.NewDecoder(res.Body)
+	var r rolesResult
+	if res.StatusCode != 200 {
+		var er errorsResult
+		err = decoder.Decode(&er)
+		if err == nil {
+			err = errors.New(strings.Join(er.Errors, "\n"))
+		}
+	} else {
+		err = decoder.Decode(&r)
+	}
+	if err != nil {
+		return nil, err
+	}
+	return r.Roles, nil
+}
