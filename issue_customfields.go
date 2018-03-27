2c87f256a8efe7ce2574f6f7674010ae184d0ed0
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2018-03-27 18:30:05 -0300

    Add method CustomFields

---
+++ issue_customfields.go
@@ -0,0 +1,51 @@
+package redmine
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+type customFieldsResult struct {
+	CustomFields []CustomField `json:"custom_fields"`
+}
+
+// CustomFields consulta los campos personalizados
+func (c *Client) CustomFields() ([]CustomField, error) {
+	req, err := http.NewRequest(
+		"GET",
+		fmt.Sprintf("%s/custom_fields.json?%s",
+			c.endpoint,
+			c.getPaginationClause()),
+		nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("X-Redmine-API-Key", c.apikey)
+
+	res, err := c.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	decoder := json.NewDecoder(res.Body)
+	var r customFieldsResult
+	if res.StatusCode != 200 {
+		var er errorsResult
+		err = decoder.Decode(&er)
+		if err == nil {
+			err = errors.New(strings.Join(er.Errors, "\n"))
+		}
+	} else {
+		err = decoder.Decode(&r)
+	}
+	if err != nil {
+		return nil, err
+	}
+	return r.CustomFields, nil
+}
