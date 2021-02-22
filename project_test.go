fb4d0ef393500df92161a6ac77fdc9ad5dbfcaf3
Philipp Pixel <pixeloffice@philipppixel.de>
2021-02-22 09:54:13 +0100

    #1 Test parsable additional project field structs: trackers and enabled_modules

--- project_test.go
+++ project_test.go
@@ -1,6 +1,13 @@
 package redmine
 
-import "testing"
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
 func Test_validateAdditionalFields(t *testing.T) {
 	type args struct {
@@ -62,4 +69,74 @@ func Test_additionalFieldsParam(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestClient_ProjectWithAdditionalFields(t *testing.T) {
+	t.Run("should parse general project fields, module names, and trackers from http response", func(t *testing.T) {
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			_, _ = fmt.Fprintln(w, `{
+  "project": {
+    "id": 1,
+    "name": "example project",
+    "identifier": "exampleproject",
+    "description": "This is an example project.",
+    "homepage": "http://github.com/cloudogu/go-redmine",
+    "status": 1,
+    "is_public": true,
+    "inherit_members": true,
+    "trackers": [
+      {
+        "id": 1,
+        "name": "Bug"
+      },
+      {
+        "id": 2,
+        "name": "Feature"
+      }
+    ],
+    "enabled_modules": [
+      {
+        "id": 71,
+        "name": "issue_tracking"
+      },
+			{
+        "id": 73,
+        "name": "wiki"
+      }
+    ],
+    "created_on": "2021-02-19T16:51:03Z",
+    "updated_on": "2021-02-19T16:51:25Z"
+  }
+}`)
+		}))
+		defer ts.Close()
+
+		sut := NewClient(ts.URL, "apiKey")
+
+		actualProject, err := sut.ProjectWithAdditionalFields(1, "enabled_modules", "trackers")
+
+		require.NoError(t, err)
+		require.NotEmpty(t, actualProject)
+		expectedProject := &Project{
+			Id:             1,
+			ParentID:       Id{},
+			Name:           "example project",
+			Identifier:     "exampleproject",
+			Description:    "This is an example project.",
+			Homepage:       "http://github.com/cloudogu/go-redmine",
+			IsPublic:       true,
+			InheritMembers: true,
+			Trackers: []Tracker{
+				{ID: 1, Name: "Bug"},
+				{ID: 2, Name: "Feature"},
+			},
+			EnabledModules: []EnabledModul{
+				{ID: 71, Name: "issue_tracking"},
+				{ID: 73, Name: "wiki"},
+			},
+			CreatedOn: "2021-02-19T16:51:03Z",
+			UpdatedOn: "2021-02-19T16:51:25Z",
+		}
+		assert.Equal(t, expectedProject, actualProject)
+	})
 }
